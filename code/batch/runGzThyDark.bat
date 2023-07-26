8add24e0cd92ade07d17e4560d16b4ab4bc850fa
Daniel Kaas Bundgaard Jørgensen <danielkj94@hotmail.com>
2023-07-26 16:27:18 +0200

    Batch - Run gzdoom.exe with Parameters 📜🗑️🆒
    It has been cumbersome during development of my custom Doom Episode, 
    to select several files (DOOM.WAD & DOOM2.WAD) and drag them into the gzdoom.exe, 
    especially with other mods on top (such as LiTDoom), to do some playtesting 🚧

---
+++ code/batch/runGzThyDark.bat
@@ -0,0 +1,8 @@
+@REM .\gzdoom.exe -file DarkRoomFinal_2507_July.wad
+
+@REM .\gzdoom.exe -iwad DOOM2.WAD -iwad DOOM.WAD -file DarkRoomFinal_2507_July.wad
+
+@REM .\gzdoom.exe -iwad DOOM.WAD DOOM2.WAD -file DarkRoomFinal_2507_July.wad
+
+
+.\gzdoom.exe -iwad DOOM.WAD -file DOOM2.WAD DarkRoomFinal_2507_July.wad
